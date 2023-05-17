28fd3e43daf5e6eb690d6eab6599d4f528fc4f38
Nicole Vu <nicole.vu@kellogg.northwestern.edu>
2023-05-17 00:46:46 +0000

    Attempt #1 at linking places & posts

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -3,5 +3,31 @@ class PlacesController < ApplicationController
   def index
     @places = Place.all
   end
+
+  def new
+  # renders places/new view with form to create a new place
+    @place = Place.new
+  end
+
+  def show
+  # find a place
+    @place = Place.find_by({"id" => params["id"]})
+    @posts = Post.where({"place_id" => @place["id"]})
+  end
+
+  def create 
+  # start with a new place
+    @place = Place.new
+
+  # assign user-entered form data to place table's columns
+    @place["name"] = params["place"]["name"]
   
+  # save new place row
+    @place.save
+  
+  # redirect user
+    redirect_to "/places"
+  
+  end
+
 end
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,24 @@
 class PostsController < ApplicationController
-
-  def index
-    @posts = Post.all
+  
+  def new
+    @post = Post.new
   end
 
+  def create 
+    # start with a new post
+      @post = Post.new
+  
+    # assign user-entered form data to place table's columns
+      @post["title"] = params["post"]["title"]
+      @post["posted_on"] = params["post"]["posted_on"]
+      @post["description"] = params["post"]["description"]
+      @post["place_id"] = params["post"]["place_id"]
+    
+    # save new place row
+      @post.save
+    
+    # redirect user
+      redirect_to "/places/#{@post["place_id"]}"
+    
+    end
 end
